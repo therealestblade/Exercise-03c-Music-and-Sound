31dfca9194207b6ea4e476a556f394630c6f3c33
therealestblade <89621761+therealestblade@users.noreply.github.com>
2021-09-30 18:51:23 -0400

    Completes Exercise

--- Pieces/Piece.gd
+++ Pieces/Piece.gd
@@ -15,8 +15,10 @@ export var transparent_time = 1.0
 export var scale_time = 1.5
 export var rot_time = 1.5
 
-
-
+var sound_1 = null
+var sound_2 = null
+var sound_3 = null
+var Coin = preload("res://Coin/Coin.gd")
 func _ready():
 	$Select.texture = $Sprite.texture
 	$Select.scale = $Sprite.scale
@@ -36,9 +38,26 @@ func _physics_process(_delta):
 func generate(pos):
 	position = Vector2(pos.x,-100)
 	target_position = pos
-
+	if sound_1 == null:
+		sound_1 = get_node_or_null("/root/Game/1")
+	if sound_1 != null:
+		sound_1.play()
 func move_piece(change):
 	target_position = target_position + change
+	if sound_2 == null:
+		sound_2 = get_node_or_null("/root/Game/1")
+	if sound_2 != null:
+		sound_2.play()
 
 func die():
 	dying = true;
+	if sound_3 == null:
+		sound_3 = get_node_or_null("/root/Game/1")
+	if sound_3 != null:
+		sound_3.play()
+		if Effects == null:
+			Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var coin = Coin.instance()
+		coin.position = target_position
+		Effects.add_child(coin)
